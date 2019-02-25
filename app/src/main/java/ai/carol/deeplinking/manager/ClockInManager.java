43aa6459c4326c5d640ff45085c96a50e45c8c35
Bruno Tortato Furtado <bruno@furtado.me>
2019-02-24 21:47:19 -0300

    [Changed] New methods to send/receive data

--- app/src/main/java/ai/carol/deeplinking/manager/ClockInManager.java
+++ app/src/main/java/ai/carol/deeplinking/manager/ClockInManager.java
@@ -1,5 +1,6 @@
 package ai.carol.deeplinking.manager;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -7,26 +8,13 @@ import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
 import java.util.List;
 
+import ai.carol.deeplinking.helper.AlertHelper;
+import ai.carol.deeplinking.helper.ConverterHelper;
 import ai.carol.deeplinking.model.ClockInObject;
 
 public final class ClockInManager {
-
-    private static final String CLOCK_IN = "clockin";
-    private static final String AUTHORITY_LOGIN = "login";
-    private static final String PATH_OAUTH2 = "oauth2";
-    private static final String PARAM_TENANT = "tenant";
-    private static final String PARAM_EMAIL = "email";
-    private static final String PARAM_PASSWORD = "password";
-    private static final String PARAM_APP_SCHEME = "appScheme";
-    private static final String PARAM_APP_NAME = "appName";
-    private static final String PARAM_APP_IDENTIFIER = "appIdentifier";
-    private static final String PARAM_DATA = "data";
 
     private final Context mContext;
 
@@ -34,80 +22,63 @@ public final class ClockInManager {
         mContext = context;
     }
 
-    //region - Public
-
-    public Intent buildIntent(@NonNull final String tenant,
-                              @NonNull final String email,
-                              @NonNull final String password,
-                              @NonNull final String appScheme,
-                              @NonNull final String appName,
-                              @NonNull final String appIdentifier) {
+    public void startClockInActivity(@NonNull final Activity activity,
+                                     @NonNull final String tenant,
+                                     @NonNull final String email,
+                                     @NonNull final String password,
+                                     @NonNull final String appScheme,
+                                     @NonNull final String appName,
+                                     @NonNull final String appIdentifier) {
         Uri.Builder builder = new Uri.Builder()
-                .scheme(CLOCK_IN)
-                .authority(AUTHORITY_LOGIN)
-                .appendPath(PATH_OAUTH2)
-                .appendQueryParameter(PARAM_TENANT, tenant)
-                .appendQueryParameter(PARAM_EMAIL, email)
-                .appendQueryParameter(PARAM_PASSWORD, password)
-                .appendQueryParameter(PARAM_APP_SCHEME, appScheme)
-                .appendQueryParameter(PARAM_APP_NAME, appName)
-                .appendQueryParameter(PARAM_APP_IDENTIFIER, appIdentifier);
+                .scheme("clockin")
+                .authority("login")
+                .appendPath("oauth2")
+                .appendQueryParameter("tenant", tenant)
+                .appendQueryParameter("email", email)
+                .appendQueryParameter("password", password)
+                .appendQueryParameter("appScheme", appScheme)
+                .appendQueryParameter("appName", appName)
+                .appendQueryParameter("appIdentifier", appIdentifier);
 
         final Uri uri = builder.build();
 
-        final Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent clockInIntent = new Intent(Intent.ACTION_VIEW, uri);
+        clockInIntent = clockInIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NO_HISTORY);
 
-        if (!isIntentSave(intent)) {
+        final PackageManager packageManager = mContext.getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(clockInIntent, 0);
+
+        if (activities.size() > 0) {
+            activity.startActivity(clockInIntent);
+        } else {
+            AlertHelper.showPlayStoreAlert(activity);
+        }
+    }
+
+    public List<ClockInObject> getClockIns(final Intent intent) {
+        if (intent == null) {
             return null;
         }
 
-        return intent;
-    }
-
-    public String receivedData(@NonNull  final Intent intent) {
         final Uri intentData = intent.getData();
         if (intentData == null) {
             return null;
         }
 
+        // IMPORTANT - reset the data
+        intent.setData(null);
+
         final String host = intentData.getHost();
-        if (host == null || !host.equals(CLOCK_IN)) {
+        if (host == null || !host.equals("clockin")) {
             return null;
         }
 
-        final String data = intentData.getQueryParameter(PARAM_DATA);
-        if (data == null) {
+        final String clockInsStr = intentData.getQueryParameter("data");
+        if (clockInsStr == null) {
             return null;
         }
 
-        return data;
+        return ConverterHelper.getClockInsFromString(clockInsStr);
     }
-
-    public List<ClockInObject> serializeReceivedData(final String data) {
-        if (data == null) {
-            return null;
-        }
-
-        final Type type = new TypeToken<List<ClockInObject>>(){}.getType();
-        return new Gson().fromJson(data, type);
-    }
-
-    //endregion
-
-    //region - Private
-
-    private boolean isIntentSave(final Intent intent) {
-        if (intent == null) {
-            return false;
-        }
-
-        final PackageManager packageManager = mContext.getPackageManager();
-        List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, 0);
-
-        return activities.size() > 0;
-    }
-
-    //endregion
 
 }
