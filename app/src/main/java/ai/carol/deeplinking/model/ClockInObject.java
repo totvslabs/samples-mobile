85385da17091040d798b05d70713dd024e26ad46
Bruno Tortato Furtado <bruno@furtado.me>
2019-02-24 21:46:58 -0300

    [Changed] ClockIn object mapper

--- app/src/main/java/ai/carol/deeplinking/model/ClockInObject.java
+++ app/src/main/java/ai/carol/deeplinking/model/ClockInObject.java
@@ -5,104 +5,25 @@ import com.google.gson.annotations.SerializedName;
 public final class ClockInObject {
 
     @SuppressWarnings("unused")
-    @SerializedName("clockinCoordinates")
-    private String clockinCoordinates;
+    @SerializedName("name")
+    private String name;
 
     @SuppressWarnings("unused")
-    @SerializedName("clockinDatetime")
-    private String clockinDatetime;
+    @SerializedName("data")
+    private ClockInDataObject data;
 
-    @SuppressWarnings("unused")
-    @SerializedName("clockinDatetimeStr")
-    private String clockinDatetimeStr;
-
-    @SuppressWarnings("unused")
-    @SerializedName("clockinMode")
-    private int clockinMode;
-
-    @SuppressWarnings("unused")
-    @SerializedName("clockinNSRNumber")
-    private int clockinNSRNumber;
-
-    @SuppressWarnings("unused")
-    @SerializedName("deviceCode")
-    private String deviceCode;
-
-    @SuppressWarnings("unused")
-    @SerializedName("employeePersonId")
-    private String employeePersonId;
-
-    @SuppressWarnings("unused")
-    @SerializedName("clockinImage")
-    private String clockinImage;
-
-    @SuppressWarnings("unused")
-    @SerializedName("locationCode")
-    private String locationCode;
-
-    @SuppressWarnings("unused")
-    @SerializedName("smsSentOnDateTime")
-    private String smsSentOnDateTime;
-
-    @SuppressWarnings("unused")
-    @SerializedName("supervisorPersonId")
-    private String supervisorPersonId;
 
     private ClockInObject() { }
 
+
     @SuppressWarnings("unused")
-    public String getClockinCoordinates() {
-        return clockinCoordinates;
+    public String getName() {
+        return name;
     }
 
     @SuppressWarnings("unused")
-    public String getClockinDatetime() {
-        return clockinDatetime;
-    }
-
-    @SuppressWarnings("unused")
-    public String getClockinDatetimeStr() {
-        return clockinDatetimeStr;
-    }
-
-    @SuppressWarnings("unused")
-    public int getClockinMode() {
-        return clockinMode;
-    }
-
-    @SuppressWarnings("unused")
-    public int getClockinNSRNumber() {
-        return clockinNSRNumber;
-    }
-
-    @SuppressWarnings("unused")
-    public String getDeviceCode() {
-        return deviceCode;
-    }
-
-    @SuppressWarnings("unused")
-    public String getEmployeePersonId() {
-        return employeePersonId;
-    }
-
-    @SuppressWarnings("unused")
-    public String getClockinImage() {
-        return clockinImage;
-    }
-
-    @SuppressWarnings("unused")
-    public String getLocationCode() {
-        return locationCode;
-    }
-
-    @SuppressWarnings("unused")
-    public String getSmsSentOnDateTime() {
-        return smsSentOnDateTime;
-    }
-
-    @SuppressWarnings("unused")
-    public String getSupervisorPersonId() {
-        return supervisorPersonId;
+    public ClockInDataObject getData() {
+        return data;
     }
 
 }
