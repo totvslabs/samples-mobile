0e641a460c96b92817b2c26d1e09758d31129853
Jansel Rodriguez <jvra16@gmail.com>
2020-06-17 13:50:30 -0400

    refactoring: component names

--- react-native-totvs-clockin-vision/src/index.js
+++ react-native-totvs-clockin-vision/src/index.js
@@ -1,15 +1,15 @@
 /**
  * Export [VisionModule]
  */
-export { default as VisionModule } from './modules/VisionModule';
+export { default as VisionModule } from './modules/vision';
 /**
  * Export [FaceCameraView]
  */
-export { default as FaceCameraView, FaceCameraConstants, FaceCameraState } from './views/FaceCameraView';
+export { default as FaceCameraView, FaceCameraConstants, FaceCameraState } from './views/Face';
 /**
  * Export [BarcodeCameraView]
  */
-export { default as BarcodeCameraView, BarcodeCameraConstants, BarcodeCameraState } from './views/BarcodeCameraView';
+export { default as BarcodeCameraView, BarcodeCameraConstants, BarcodeCameraState } from './views/Barcode';
 /**
  * Import & Export camera state
  */
