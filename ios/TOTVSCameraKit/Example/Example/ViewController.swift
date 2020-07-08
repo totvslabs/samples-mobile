71bff350629e0da3e5c19abbe8a847b6d24caca4
Jansel Rodriguez <jvra16@gmail.com>
2020-07-08 00:11:32 -0400

    fixing: data analysis main didpatch queue blocking

---
+++ ios/TOTVSCameraKit/Example/Example/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Jansel Rodriguez on 7/6/20.
+//  Copyright © 2020 Jansel. All rights reserved.
+//
+
+import UIKit
+import TOTVSCameraKit
+import FirebaseMLVision
+
+class ViewController: UIViewController {
+    
+    private var cameraView: CameraView!
+    
+    private lazy var vision = Vision.vision()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initCameraView()
+    }
+    
+    @IBAction func capture(_ button: UIButton) {
+        cameraView.takePicture(with: OutputFileOptions()) { (url, error) in
+            print("saved")
+        }
+    }
+    
+    @IBAction func flipCamera(_ button: UIButton) {
+        cameraView.toggleCamera()    
+    }
+}
+
+/// MARK: Init CameraView
+extension ViewController {
+    private func initCameraView() {
+        cameraView = CameraView()
+        view.insertSubview(cameraView, at: 0)
+        
+        cameraView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cameraView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            cameraView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            cameraView.topAnchor.constraint(equalTo: view.topAnchor),
+            cameraView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension ViewController {
+    private func faceDetector() {
+    }
+}
