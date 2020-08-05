2b3555151611108840aadbff3748924377277729
Jansel Rodriguez <jvra16@gmail.com>
2020-08-04 23:37:11 -0400

    adding: objc c++ bridge ffor swift code

---
+++ react-native/react-native-totvs-clockin-vision-app/ios/recognition/ObjcFaceRecognizer.h
@@ -0,0 +1,20 @@
+//
+//  ObjcFaceRecognizer.h
+//  ReactNativeTotvsClockinVisionApp
+//
+//  Created by Jansel Rodriguez on 8/4/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ This file is added only for the purpose of bridging the c++ code to objc and objc to swift since
+ Swift can't access directly c++ code.
+ */
+@interface ObjcFaceRecognizer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ react-native/react-native-totvs-clockin-vision-app/ios/recognition/ObjcFaceRecognizer.mm
@@ -0,0 +1,22 @@
+//
+//  ObjcFaceRecognizer.m
+//  ReactNativeTotvsClockinVisionApp
+//
+//  Created by Jansel Rodriguez on 8/4/20.
+//
+
+#import "ObjcFaceRecognizer.h"
+#import "FaceRecognizer.h"
+
+/**
+  This file is added only for the purpose of bridging the c++ code to objc and objc to swift since
+  Swift can't access directly c++ code.
+*/
+
+@interface ObjcFaceRecognizer ()
+
+@end
+
+@implementation ObjcFaceRecognizer
+
+@end
